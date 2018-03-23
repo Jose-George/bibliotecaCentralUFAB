b93fcd6733e72a161e5ef723fcbd6b57a495730b
JoseGeorge <george.djc@gmail.com>
2018-03-23 09:55:04 -0300

    Refatorando o código e adicionando o Padrão MVC
    Foi retirado o banco de dados, que começou a ser implementando no commit anterior, a adição do BD ao sistema, só será feita no futuro.

--- biblioteca/src/main/java/br/edu/ufab/biblioteca/App.java
+++ biblioteca/src/main/java/br/edu/ufab/biblioteca/App.java
@@ -1,19 +1,12 @@
 package br.edu.ufab.biblioteca;
 
-import biblioteca.conexao.Conexao;
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        /*Testando base de dados*/
-        Conexao con = new Conexao(); 
-        con.getConnection();
-        
-    }	
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
 }
