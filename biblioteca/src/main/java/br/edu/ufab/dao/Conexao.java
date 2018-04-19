eb86e856281ffda27c6da99b8457d42109da2cfa
Caio <caiosant10@gmail.com>
2018-04-19 17:23:21 -0300

    Criando logs
    Criando os logs nas classes br.edu.ufab.dao.Conexao e br.edu.ufab.ao.curso.CursoDAO

--- biblioteca/src/main/java/br/edu/ufab/dao/Conexao.java
+++ biblioteca/src/main/java/br/edu/ufab/dao/Conexao.java
@@ -4,21 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.apache.tools.ant.Main;
+import org.apache.log4j.Logger;
+
+import br.edu.ufab.dao.curso.CursoDAO;
+
+
+//import org.apache.tools.ant.Main;
 
 public class Conexao {
 
 	public static Connection getConexao() {
+		Logger logger = Logger.getLogger(CursoDAO.class);		
 
 		Connection conn = null;
+		
+		logger.info("iniciando conexão com Banco de dados");
 
 		try {
 			String url = "jdbc:mysql://localhost/biblioteca";
 			conn = DriverManager.getConnection(url, "root", "");
-			System.out.println("Conexão aberta!");
+			logger.info("Conexão aberta!");
+			//System.out.println("Conexão aberta!");
 
 		} catch (SQLException e) {
-			System.err.println("Falha ao conectar com banco de dados\n" + e.getMessage());
+			logger.error("Falha ao conectar com banco de dados\n", e);
+			//System.err.println("Falha ao conectar com banco de dados\n" + e.getMessage());
 		}
 		return conn;
 	}
--- biblioteca/src/main/java/br/edu/ufab/dao/curso/CursoDAO.java
+++ biblioteca/src/main/java/br/edu/ufab/dao/curso/CursoDAO.java
@@ -2,24 +2,30 @@ package br.edu.ufab.dao.curso;
 
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
 import br.edu.ufab.dao.Conexao;
-import br.edu.ufab.model.acervo.Revista;
+//import br.edu.ufab.model.acervo.Revista;
 import br.edu.ufab.model.curso.Curso;
 
 public class CursoDAO {
 
 	private Connection conexao;
 	PreparedStatement stmt;
+	
+	private static Logger logger = Logger.getLogger(CursoDAO.class);
 
 	public CursoDAO() {
 		this.conexao = (Connection) Conexao.getConexao();
 	}
 
 	public boolean insertion(Curso curso) {
-
+		logger.info("Inserindo curso na base de dados, passando"+curso+"como parametro");
+		
+		boolean flag = false;
 		String sql = "INSERT INTO curso(nome,areaCurso,tipo) VALUES(?,?,?)";
 
 		try {
@@ -30,10 +36,14 @@ public class CursoDAO {
 	
 			stmt.execute();
 			stmt.close();
-			return true;
+			logger.info("Inseriu "+curso+" com sucesso!");
+			flag = true;
+			return flag;
 
 		} catch (SQLException u) {
-			throw new RuntimeException(u);
+			//throw new RuntimeException(u);
+			logger.error("Falha ao inserir "+curso+" na base de dados", u);
+			return flag;
 		}
 
 	}
