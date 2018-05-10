00c6f44b8daf49f35751710f73431097428c18c8
JoseGeorge <george.djc@gmail.com>
2018-05-10 19:52:33 -0300

    Front-End:: Criação de algumas páginas HTML/CSS

--- biblioteca/src/main/java/br/edu/ufab/dao/aluno/AlunoDAO.java
+++ biblioteca/src/main/java/br/edu/ufab/dao/aluno/AlunoDAO.java
@@ -85,22 +85,21 @@ public class AlunoDAO {
 	
 	public boolean update(Aluno aluno){
 		
-		String sql = "UPDATE aluno nome=?,nomeMae=?,rg=?,naturalidade=?,endereco=?"
-				+ ",telefone=?,email=?,senha=?,anoIngresso=?,periodoIngresso=? WHERE cpf = ? ";
+		String sql = "UPDATE aluno nome=?, nomeMae=?, rg=?, naturalidade=?, endereco=? "
+				+ ", telefone=?, email=?, senha=?, anoIngresso=?, periodoIngresso=? WHERE cpf = ? ";
 		try {
 			stmt = (PreparedStatement) conexao.prepareStatement(sql);
 			stmt.setString(1, aluno.getNome());
 			stmt.setString(2, aluno.getNomeMae());
-			stmt.setString(3, aluno.getCpf());
-			stmt.setString(4, aluno.getRg());
-			stmt.setString(5, aluno.getNaturalidade());
-			stmt.setString(6, aluno.getEndereco());
-			stmt.setString(7, aluno.getTelefone());
-			stmt.setString(8, aluno.getEmail());
-			stmt.setString(9, aluno.getSenha());
-			stmt.setString(10, aluno.getCurso());
-			stmt.setInt(11, aluno.getAnoIngresso());
-			stmt.setString(12, aluno.getPeriodoInrgresso());
+			stmt.setString(3, aluno.getRg());
+			stmt.setString(4, aluno.getNaturalidade());
+			stmt.setString(5, aluno.getEndereco());
+			stmt.setString(6, aluno.getTelefone());
+			stmt.setString(7, aluno.getEmail());
+			stmt.setString(8, aluno.getSenha());
+			stmt.setInt(9, aluno.getAnoIngresso());
+			stmt.setString(10, aluno.getPeriodoInrgresso());
+			stmt.setString(11, aluno.getCpf());
 			stmt.execute();
 			stmt.close();
 			
